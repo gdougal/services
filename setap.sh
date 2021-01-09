28599fc6c63c18d82fd8d2962fe6eea4a100e181
gdougal <gdougal@student.21-school.ru>
2021-01-09 17:55:14 +0300

    phpmyadmin

--- setap.sh
+++ setap.sh
@@ -7,9 +7,11 @@ kubectl apply -f configmap.yaml && \
 eval $(minikube docker-env) && \
 docker build -t mysql mysql/. && \
 docker build -t wordpress_image wordpress/. && \
+docker build -t php_image phpmyadmin/. && \
 docker build -t nginx_image nginx/. && \
 kubectl apply -f mysql/mysql.yaml && \
 kubectl apply -f wordpress/wordpress.yaml && \
+kubectl apply -f phpmyadmin/phpmyadmin.yaml && \
 kubectl apply -f nginx/nginx.yaml && \
 minikube dashboard &
 
