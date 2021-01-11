6a6242d0b872ede6899e10893cdbf3f9c9ae2cf7
gdougal <gdougal@student.21-school.ru>
2021-01-11 22:38:04 +0300

    some troubles with mysql

--- mysql/DB.sh
+++ mysql/DB.sh
@@ -7,6 +7,10 @@ CREATE DATABASE DB;
 CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON DB.* TO 'admin';
 FLUSH PRIVILEGES;
+CREATE USER 'user1'@'%' IDENTIFIED BY 'neadmin';
+CREATE USER 'user2'@'%' IDENTIFIED BY 'neadmin';
+CREATE USER 'user3'@'%' IDENTIFIED BY 'neadmin';
+CREATE USER 'user4'@'%' IDENTIFIED BY 'neadmin';
 MYSQL_SCRIPT
 
 /etc/init.d/mariadb stop
